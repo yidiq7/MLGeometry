1d2f29bd5f7ff6a0c5e043b4a77845dcd05ecdea
Yidi Qi <qiyidi2012@gmail.com>
2023-02-20 15:37:44 -0500

    More documentation for the complex hessian

--- MLGeometry/complex_math.py
+++ MLGeometry/complex_math.py
@@ -1,3 +1,37 @@
+"""Defines a function to calculate the complex hessian
+
+(The following comment is written in markdown)
+
+In Tensorflow, `tf.gradient` with respect to a complex variable is defined as 
+$2\frac{\partial Real(f)}{\partial \bar{z}}$ for practical reasons.
+To obtain the gradient with the mathematical definition, rewrite 
+$\frac{\partial f}{\partial \bar{z}}$ as $\frac{\partial Real(f)}{\partial \bar{z}} +
+\frac{\partial Imag(f)}{\partial \bar{z}}$, and calculate each term using `tf.gradients`
+
+To obtain the complex hessian dzdzbar(f), where f is a real function,
+first calculate ∂f/∂z by taking conjugate of `tf.gradient`, then take
+the second derivative with `gradients_zbar`. By default, Tensorflow can only 
+handle the gradients of a list of scalar with respect to anther list of scalar. 
+So the list of vectors from the first derivative need to be unstacked to lists of 
+scalars, then take the second derivative and stack them back.
+
+A simple test of the result is to calculate the hessian of z0z0bar + z1z1bar:
+
+    @tf.function
+    def inner_product(x):
+        a = tf.math.real(tf.reduce_sum(x*tf.math.conj(x),1))
+        return mlg.complex_math.complex_hessian(a, x)
+
+    foo = tf.convert_to_tensor([[1.0-1.0j, 2.0+2.0j]], dtype=tf.complex64)
+    print(inner_product(foo))
+
+The result is indeed as expected
+
+    tf.Tensor(
+    [[[1.+0.j 0.+0.j]
+     [0.+0.j 1.+0.j]]], shape=(1, 2, 2), dtype=complex64)
+"""
+
 import tensorflow as tf
 
 def gradients_zbar(func, x):
