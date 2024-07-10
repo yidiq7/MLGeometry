82cf0f45c796ca19d49f1cb1453971d3670e13d6
Marek Cerny <me@marekcerny.com>
2024-07-10 15:54:11 +0000

    add tahn

--- training_u1/models.py
+++ training_u1/models.py
@@ -36,3 +36,36 @@ class u1_model_relu(tf.keras.Model):
         x = self.outer_layers(x)
         x = tf.math.log(x)
         return x
+
+
+class u1_model_tanh(tf.keras.Model):
+
+    def __init__(self, n_units, m_units, g_steps=8):
+        super().__init__()
+        assert len(n_units) > 0
+        assert len(m_units) > 0
+
+        inner_layers = tf.keras.Sequential([
+            tf.keras.layers.Dense(
+                n_units[i],
+                input_shape=((kINPUT_DIM, ) if i == 0 else (n_units[i - 1], )),
+                activation=('tanh' if i < len(n_units) - 1 else None)
+            )
+            for i in range(len(n_units))
+        ])
+
+        self.u1_layer = U1EquivariantLayer(inner_layers, g_steps=g_steps)
+
+        self.outer_layers = tf.keras.Sequential([
+            tf.keras.layers.Dense(
+                m_units[i],
+                activation=('tanh' if i < len(m_units) - 1 else None)
+            )
+            for i in range(len(m_units))
+        ])
+
+    def call(self, inputs):
+        x = self.u1_layer(inputs)
+        x = self.outer_layers(x)
+        x = tf.math.log(x)
+        return x
--- training_u1/u1equiv_train.sh
+++ training_u1/u1equiv_train.sh
@@ -10,18 +10,19 @@ mkdir -p cache
 
 for psi in 0.5 ; do
     for loss_func in "weighted_MAPE" ; do
-        for layers in "20_20-300_300_300_1"; do
+        for layers in "16_16_16-128_128_128_1"; do
             python u1equiv_train.py --seed 1234 \
-                                     --n_pairs 100000\
-                                     --batch_size 5000\
+                                     --n_pairs 100000 \
+                                     --batch_size 10000 \
                                      --function "f0" \
                                      --psi $psi \
                                      --layers $layers \
+                                     --model_name u1_model_tanh \
                                      --g_steps 16 \
                                      --save_dir "experiments-marek" \
                                      --save_name "${layers}" \
-                                     --optimizer 'adam'\
-                                     --learning_rate 0.001 \
+                                     --optimizer 'adam' \
+                                     --learning_rate 0.0005 \
                                      --decay_rate 1 \
                                      --max_epochs 1000 \
                                      --loss_func ${loss_func} \
